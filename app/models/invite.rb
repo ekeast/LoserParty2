4235b26a551c7951bbaf13ec93c6c27592c45eb1
ekeast <ekeast328@gmail.com>
2017-03-08 12:44:42 -0500

    cannot invite same person more than once

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -3,11 +3,11 @@ class Invite < ApplicationRecord
   belongs_to :sender, :class_name => 'User'
   belongs_to :recipient, :class_name => 'User'
   validate :cannot_have_guestship
-
+  validates :email, :uniqueness => true
 
     def cannot_have_guestship
       if (Guestship.where(email: email, event: event_id).first)
-        errors.add(:email, "Guest has already accepted an invite")
+        errors.add(:email, "has been taken by a guest already attending")
       end
     end
 end
